b9c15f197945158f44052ee3a579a25a6f96ebfd
Hossam <hossamsoliuman@gmail.com>
2024-08-05 02:44:24 +0300

    improving filters design and logic

--- resources/views/users/products.blade.php
+++ resources/views/users/products.blade.php
@@ -42,13 +42,13 @@
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Gender</h3>
                     <label class="block">
-                        <input type="checkbox" name="gender[]" value="male" class="mr-2"> Men
+                        <input type="checkbox" name="gender[]" value="male" class="mr-2" {{ in_array('male', request('gender', [])) ? 'checked' : '' }}> Men
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="gender[]" value="female" class="mr-2"> Women
+                        <input type="checkbox" name="gender[]" value="female" class="mr-2" {{ in_array('female', request('gender', [])) ? 'checked' : '' }}> Women
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="gender[]" value="all" class="mr-2"> All
+                        <input type="checkbox" name="gender[]" value="all" class="mr-2" {{ in_array('all', request('gender', [])) ? 'checked' : '' }}> All
                     </label>
                 </div>
 
@@ -56,19 +56,19 @@
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Categories</h3>
                     <label class="block">
-                        <input type="checkbox" name="category[]" value="Shirts" class="mr-2"> Shirts
+                        <input type="checkbox" name="category[]" value="Shirts" class="mr-2" {{ in_array('Shirts', request('category', [])) ? 'checked' : '' }}> Shirts
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="category[]" value="Hoodies" class="mr-2"> Hoodies
+                        <input type="checkbox" name="category[]" value="Hoodies" class="mr-2" {{ in_array('Hoodies', request('category', [])) ? 'checked' : '' }}> Hoodies
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="category[]" value="Pants" class="mr-2"> Pants
+                        <input type="checkbox" name="category[]" value="Pants" class="mr-2" {{ in_array('Pants', request('category', [])) ? 'checked' : '' }}> Pants
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="category[]" value="Shoes" class="mr-2"> Shoes
+                        <input type="checkbox" name="category[]" value="Shoes" class="mr-2" {{ in_array('Shoes', request('category', [])) ? 'checked' : '' }}> Shoes
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="category[]" value="Glasses" class="mr-2"> Glasses
+                        <input type="checkbox" name="category[]" value="Glasses" class="mr-2" {{ in_array('Glasses', request('category', [])) ? 'checked' : '' }}> Glasses
                     </label>
                 </div>
 
@@ -76,31 +76,31 @@
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Price Range</h3>
                     <div class="flex">
-                        <input type="number" id="minPrice" name="min_price" placeholder="Min" class="w-1/2 bg-dark-blue text-light-gray border-2 border-medium-blue rounded-lg p-2 mr-2">
-                        <input type="number" id="maxPrice" name="max_price" placeholder="Max" class="w-1/2 bg-dark-blue text-light-gray border-2 border-medium-blue rounded-lg p-2">
+                        <input type="number" id="minPrice" name="min_price" placeholder="Min" class="w-1/2 bg-dark-blue text-light-gray border-2 border-medium-blue rounded-lg p-2 mr-2" value="{{ request('min_price') }}">
+                        <input type="number" id="maxPrice" name="max_price" placeholder="Max" class="w-1/2 bg-dark-blue text-light-gray border-2 border-medium-blue rounded-lg p-2" value="{{ request('max_price') }}">
                     </div>
                 </div>
 
                 <!-- Minimum Rating Filter -->
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Minimum Rating</h3>
-                    <input type="number" id="rating" name="min_rating" min="1" max="5" step="1" class="w-full bg-dark-blue text-light-gray border-2 border-medium-blue rounded-lg p-2">
+                    <input type="number" id="rating" name="min_rating" min="1" max="5" step="1" class="w-full bg-dark-blue text-light-gray border-2 border-medium-blue rounded-lg p-2" value="{{ request('min_rating') }}">
                 </div>
 
                 <!-- Size Filter -->
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Size</h3>
                     <label class="block">
-                        <input type="checkbox" name="size[]" value="S" class="mr-2"> Small
+                        <input type="checkbox" name="size[]" value="S" class="mr-2" {{ in_array('S', request('size', [])) ? 'checked' : '' }}> Small
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="size[]" value="M" class="mr-2"> Medium
+                        <input type="checkbox" name="size[]" value="M" class="mr-2" {{ in_array('M', request('size', [])) ? 'checked' : '' }}> Medium
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="size[]" value="L" class="mr-2"> Large
+                        <input type="checkbox" name="size[]" value="L" class="mr-2" {{ in_array('L', request('size', [])) ? 'checked' : '' }}> Large
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="size[]" value="XL" class="mr-2"> Extra Large
+                        <input type="checkbox" name="size[]" value="XL" class="mr-2" {{ in_array('XL', request('size', [])) ? 'checked' : '' }}> Extra Large
                     </label>
                 </div>
 
@@ -108,19 +108,19 @@
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Color</h3>
                     <label class="block">
-                        <input type="checkbox" name="color[]" value="Red" class="mr-2"> Red
+                        <input type="checkbox" name="color[]" value="Red" class="mr-2" {{ in_array('Red', request('color', [])) ? 'checked' : '' }}> Red
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="color[]" value="Blue" class="mr-2"> Blue
+                        <input type="checkbox" name="color[]" value="Blue" class="mr-2" {{ in_array('Blue', request('color', [])) ? 'checked' : '' }}> Blue
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="color[]" value="Green" class="mr-2"> Green
+                        <input type="checkbox" name="color[]" value="Green" class="mr-2" {{ in_array('Green', request('color', [])) ? 'checked' : '' }}> Green
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="color[]" value="Black" class="mr-2"> Black
+                        <input type="checkbox" name="color[]" value="Black" class="mr-2" {{ in_array('Black', request('color', [])) ? 'checked' : '' }}> Black
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="color[]" value="White" class="mr-2"> White
+                        <input type="checkbox" name="color[]" value="White" class="mr-2" {{ in_array('White', request('color', [])) ? 'checked' : '' }}> White
                     </label>
                 </div>
 
@@ -128,16 +128,16 @@
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Material</h3>
                     <label class="block">
-                        <input type="checkbox" name="material[]" value="Cotton" class="mr-2"> Cotton
+                        <input type="checkbox" name="material[]" value="Cotton" class="mr-2" {{ in_array('Cotton', request('material', [])) ? 'checked' : '' }}> Cotton
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="material[]" value="Wool" class="mr-2"> Wool
+                        <input type="checkbox" name="material[]" value="Wool" class="mr-2" {{ in_array('Wool', request('material', [])) ? 'checked' : '' }}> Wool
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="material[]" value="Polyester" class="mr-2"> Polyester
+                        <input type="checkbox" name="material[]" value="Polyester" class="mr-2" {{ in_array('Polyester', request('material', [])) ? 'checked' : '' }}> Polyester
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="material[]" value="Silk" class="mr-2"> Silk
+                        <input type="checkbox" name="material[]" value="Silk" class="mr-2" {{ in_array('Silk', request('material', [])) ? 'checked' : '' }}> Silk
                     </label>
                 </div>
 
@@ -145,16 +145,16 @@
                 <div class="mb-4">
                     <h3 class="text-light-gray mb-2">Brand</h3>
                     <label class="block">
-                        <input type="checkbox" name="brand[]" value="1" class="mr-2"> Brand 1
+                        <input type="checkbox" name="brand[]" value="1" class="mr-2" {{ in_array('1', request('brand', [])) ? 'checked' : '' }}> Brand 1
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="brand[]" value="2" class="mr-2"> Brand 2
+                        <input type="checkbox" name="brand[]" value="2" class="mr-2" {{ in_array('2', request('brand', [])) ? 'checked' : '' }}> Brand 2
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="brand[]" value="3" class="mr-2"> Brand 3
+                        <input type="checkbox" name="brand[]" value="3" class="mr-2" {{ in_array('3', request('brand', [])) ? 'checked' : '' }}> Brand 3
                     </label>
                     <label class="block">
-                        <input type="checkbox" name="brand[]" value="4" class="mr-2"> Brand 4
+                        <input type="checkbox" name="brand[]" value="4" class="mr-2" {{ in_array('4', request('brand', [])) ? 'checked' : '' }}> Brand 4
                     </label>
                 </div>
 
